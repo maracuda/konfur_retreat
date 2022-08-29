37f738df346d8959348ad8dcde1e7416e54cd278
bis <bis@skbkontur.ru>
2022-08-29 11:58:16 +0500

    Работает балалайка

--- src/Api/Logic/Models/RetreatStatistics.cs
+++ src/Api/Logic/Models/RetreatStatistics.cs
@@ -2,4 +2,12 @@
 
 public class RetreatStatistics
 {
+    public Guid Id { get; set; }
+    public string PlayerOneName { get; set; }
+    public string PlayerTwoName { get; set; }
+    public DateTime StartDateTime { get; set; }
+    public DateTime EndDateTime { get; set; }
+    public long MouseDistance { get; set; }
+    public long ClicksCount { get; set; }
+    public long ShortCutsCount { get; set; }
 }
--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,3 +1,5 @@
+using Api.Logic;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<IRetreatStatisticsRepository>(new RetreatStatisticsRepository());
 
 var app = builder.Build();
 
